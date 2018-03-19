5a7917034960ac3ec2c10542ad3532170ff56d8b
Ramonfan <fqmlemon@sina.com>
2018-03-19 15:05:25 +0800

    add .gitignore

---
+++ NetworkWatchDog/NetworkWatchDog/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NetworkWatchDog
+//
+//  Created by Topband on 2018/3/19.
+//  Copyright © 2018年 Ramon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NetworkWatchDog/NetworkWatchDog/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NetworkWatchDog
+//
+//  Created by Topband on 2018/3/19.
+//  Copyright © 2018年 Ramon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
